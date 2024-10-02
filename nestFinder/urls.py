77caa6ad1c8460f37f2cdb12ac82ff7f190fac11
Md Yasin Hossain Akash <mdyhakash@gmail.com>
2024-10-03 05:46:18 +0600

    trying to connect  Tailwind

--- nestFinder/urls.py
+++ nestFinder/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from accounts import views as acc_views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', acc_views.login, name='login')
 ]
